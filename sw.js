cc30f3664025e3bde56c0f01c17d15f83190cb62
Qnghe <1303280520@qq.com>
2020-04-15 16:48:50 +0800

    修复service worker错误

--- sw.js
+++ sw.js
@@ -2,11 +2,14 @@ var cacheStorageKey = 'pwa-1';
 
 var cacheList = [
     '/',
+    'manifest.json',
     'index.html',
     'main.css',
     'app1.png',
-    '//cdn.repository.webfont.com/webfonts/nomal/115685/45807/5d6ce9dff629d813c0d4984b.gif?r=115812447414',
+    'app2.png',
 ];
+
+// 预缓存静态资源并立即激活 Service Worker
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheStorageKey).then(cache => {
@@ -15,20 +18,10 @@ self.addEventListener('install', e => {
     )
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            if (response != null) {
-                return response;
-            }
-            return fetch(e.request.url);
-        })
-    )
-});
-
+// 监听activate事件, 激活后通过cache的key来判断是否更新cache中的静态资源
 self.addEventListener('activate', function(e) {
     e.waitUntil(
-        caches.keys.then(cacheNames => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(name => {
                     if (name !== cacheStorageKey) {
@@ -36,7 +29,19 @@ self.addEventListener('activate', function(e) {
                     }
                 })
             )
+        }).then(() => self.clients.claim())
+    )
+})
+
+// 缓存策略 -- 在IOS 11.3.1 上有bug, 缓存后的音乐不能播放, 歌手图片不显示 --
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            if (response) {
+                return response;
+            }
+            return fetch(e.request.url);
         })
     )
-    return self.clients.claim();
-})
+});
+
